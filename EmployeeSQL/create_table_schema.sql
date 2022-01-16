9045719ce21f98f95aeca3b3365579a8b8cf676c
Kelvin Nguyen <k.nguyen.pwc@gmail.com>
2022-01-16 21:28:03 +1100

    modified datatype in tables

--- EmployeeSQL/create_table_schema.sql
+++ EmployeeSQL/create_table_schema.sql
@@ -1,3 +1,7 @@
+ALTER DATABASE sql_challenge 
+SET datestyle TO "ISO, MDY";
+
+
 CREATE TABLE titles(
 title_id VARCHAR(20) NOT NULL,
 title VARCHAR(30) NOT NULL,
@@ -6,53 +10,45 @@ PRIMARY KEY (title_id)
 
 
 CREATE TABLE departments(
-dept_no VARCHAR(10),
-dept_name VARCHAR(20),
+dept_no VARCHAR(10) NOT NULL,
+dept_name VARCHAR(20) NOT NULL,
 PRIMARY KEY (dept_no)
 );
 
 
 CREATE TABLE employees(
-emp_no VARCHAR(10) NOT NULL, 
+emp_no INT NOT NULL, 
 emp_title_id VARCHAR(20) NOT NULL,
-birth_date VARCHAR(10) NOT NULL,
+birth_date DATE NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 sex VARCHAR(1) NOT NULL,
-hire_date VARCHAR(10) NOT NULL,
+hire_date DATE NOT NULL,
 FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 PRIMARY KEY(emp_no));
 
 
 CREATE TABLE dept_emp(
-emp_no VARCHAR(10),
+emp_no INT NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-dept_no VARCHAR(10),
+dept_no VARCHAR(10) NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 
 CREATE TABLE dept_manager(
-dept_no VARCHAR(10),
+dept_no VARCHAR(10) NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-emp_no VARCHAR(10),
+emp_no INT NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 PRIMARY KEY (dept_no,emp_no)
 );
 
 
 CREATE TABLE salaries(
-emp_no VARCHAR(10) NOT NULL,
+emp_no INT NOT NULL,
 salary INT NOT NULL,
 FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
-
-
-
-
-
-
-
-
 
 
--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -7,7 +7,8 @@ JOIN salaries s ON (e.emp_no = s.emp_no);
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT last_name, first_name, hire_date
 FROM employees
-WHERE hire_date like '%1986';
+WHERE hire_date BETWEEN  '1986-01-01' AND '1986-12-31';
+
 
 --List the manager of each department with the following information:
 --department number, department name, the manager's employee number, last name, first name.
@@ -27,16 +28,19 @@ JOIN departments d ON (de.dept_no = d.dept_no);
 
 SELECT * FROM emp_by_dept;
 
+
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
 FROM employees
 WHERE (first_name = 'Hercules') 
 AND (last_name LIKE 'B%');
 
+
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT *
 FROM emp_by_dept
 WHERE dept_name = 'Sales';
+
 
 --List all employees in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name.
@@ -45,11 +49,13 @@ FROM emp_by_dept
 WHERE (dept_name = 'Sales')
 OR (dept_name = 'Development');
 
+
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, count(last_name) AS frequency
 FROM employees
 GROUP BY last_name
 ORDER BY frequency DESC;
+
 
 --From 1 unusual last_name, I found a fake record!
 SELECT *
